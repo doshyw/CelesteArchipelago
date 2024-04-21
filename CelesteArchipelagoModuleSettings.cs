6c6c2278e94286e2c2eb038a663f22f2c49b7b87
Aly <me@s5.pm>
2024-04-20 17:24:13 -0700

    Allow switching deathlink values while connected

--- CelesteArchipelagoModuleSettings.cs
+++ CelesteArchipelagoModuleSettings.cs
@@ -9,6 +9,29 @@ namespace Celeste.Mod.CelesteArchipelago {
         public string Password { get; set; } = "";
         public string Server { get; set; } = "";
         public string Port { get; set; } = "38281";
-        public bool DeathLink { get; set; } = false;
+
+        public bool DeathLink
+        {
+            get => _deathLink;
+            set
+            {
+                if (ArchipelagoController.Instance is not null &&
+                    ArchipelagoController.Instance.DeathLinkService is not null)
+                {
+                    switch (value)
+                    {
+                        case true when !_deathLink:
+                            ArchipelagoController.Instance.DeathLinkService.EnableDeathLink();
+                            break;
+                        case false when _deathLink:
+                            ArchipelagoController.Instance.DeathLinkService.DisableDeathLink();
+                            break;
+                    }
+                }
+                _deathLink = value;
+            }
+        }
+
+        private bool _deathLink = false;
     }
 }
