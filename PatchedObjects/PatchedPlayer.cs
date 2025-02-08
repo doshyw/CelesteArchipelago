d81fd907bbede1232d718e75aba5f25bc4019113
Timothy Su <ts63280@gmail.com>
2025-02-07 22:30:44 -0800

    namespaces, for consistency

--- PatchedObjects/PatchedPlayer.cs
+++ PatchedObjects/PatchedPlayer.cs
@@ -1,45 +1,47 @@
 using System;
 using Microsoft.Xna.Framework;
 
-namespace Celeste.Mod.CelesteArchipelago;
-
-public class PatchedPlayer : IPatchable
+namespace Celeste.Mod.CelesteArchipelago
 {
-    public void Load()
-    {
-        On.Celeste.Player.Update += Update;
-        Everest.Events.Player.OnSpawn += OnSpawn;
-        Everest.Events.Player.OnDie += OnDie;
-    }
 
-    public void Unload()
+    public class PatchedPlayer : IPatchable
     {
-        On.Celeste.Player.Update -= Update;
-        Everest.Events.Player.OnSpawn -= OnSpawn;
-        Everest.Events.Player.OnDie -= OnDie;
-    }
-
-    private static void Update(On.Celeste.Player.orig_Update orig, Player self)
-    {
-        if (ArchipelagoController.Instance.DeathLinkStatus == DeathLinkStatus.Pending)
+        public void Load()
         {
-            self.Die(Vector2.Zero, true);
+            On.Celeste.Player.Update += Update;
+            Everest.Events.Player.OnSpawn += OnSpawn;
+            Everest.Events.Player.OnDie += OnDie;
         }
-        orig.Invoke(self);
-    }
 
-    private static void OnSpawn(Player player)
-    {
-        if (ArchipelagoController.Instance.DeathLinkStatus == DeathLinkStatus.Dying)
+        public void Unload()
         {
-            ArchipelagoController.Instance.DeathLinkStatus = DeathLinkStatus.None;
+            On.Celeste.Player.Update -= Update;
+            Everest.Events.Player.OnSpawn -= OnSpawn;
+            Everest.Events.Player.OnDie -= OnDie;
         }
-    }
 
-    private static void OnDie(Player player)
-    {
-        ArchipelagoController.Instance.SendDeathLinkCallback();
-        ArchipelagoController.Instance.DeathLinkStatus = DeathLinkStatus.Dying;
-        ArchipelagoController.Instance.isLocalDeath = true;
+        private static void Update(On.Celeste.Player.orig_Update orig, Player self)
+        {
+            if (ArchipelagoController.Instance.DeathLinkStatus == DeathLinkStatus.Pending)
+            {
+                self.Die(Vector2.Zero, true);
+            }
+            orig.Invoke(self);
+        }
+
+        private static void OnSpawn(Player player)
+        {
+            if (ArchipelagoController.Instance.DeathLinkStatus == DeathLinkStatus.Dying)
+            {
+                ArchipelagoController.Instance.DeathLinkStatus = DeathLinkStatus.None;
+            }
+        }
+
+        private static void OnDie(Player player)
+        {
+            ArchipelagoController.Instance.SendDeathLinkCallback();
+            ArchipelagoController.Instance.DeathLinkStatus = DeathLinkStatus.Dying;
+            ArchipelagoController.Instance.isLocalDeath = true;
+        }
     }
 }
